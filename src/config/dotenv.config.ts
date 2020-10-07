63f3f5887628ff45cc2764755a32ea9971d5f1fc
farukbigez <60490540+farukbigez@users.noreply.github.com>
2020-10-08 00:00:29 +0300

    .dotenv file configured

--- src/config/dotenv.config.ts
+++ src/config/dotenv.config.ts
@@ -1,2 +1,9 @@
 import dotenv from 'dotenv'
 
+dotenv.config()
+export default {
+  db: process.env.DB,
+  jwtSecret: process.env.JWT_SECRET,
+  port: process.env.PORT,
+  allowedOrigins: ['http://localhost:3000']
+}
